6c0b99086dc8d527eb002c1f8041160d447ea83a
spipu <spipu@790b81a2-528e-45d7-b412-d1ce1f3ab57f>
2011-02-11 22:31:23 +0000

    convert examples in UTF8 + Zend

--- trunk/exemples/exemple07.php
+++ trunk/exemples/exemple07.php
@@ -1,34 +1,33 @@
 <?php
 /**
- * Logiciel : exemple d'utilisation de HTML2PDF
- * 
- * Convertisseur HTML => PDF
- * Distribué sous la licence LGPL. 
+ * HTML2PDF Librairy - example
  *
- * @author		Laurent MINGUET <webmaster@html2pdf.fr>
- * 
- * SetDisplayMode : permet de choisir le mode d'affichage par defaut
- * SetProtection : permet de proteger le document à l'ouverture avec un mot de passe, et de ne donner que certains droits d'action
- * 
- * isset($_GET['vuehtml']) n'est pas obligatoire
- * il permet juste d'afficher le résultat au format HTML
- * si le paramètre 'vuehtml' est passé en paramètre _GET
+ * HTML => PDF convertor
+ * distributed under the LGPL License
+ *
+ * @author      Laurent MINGUET <webmaster@html2pdf.fr>
+ *
+ * isset($_GET['vuehtml']) is not mandatory
+ * it allow to display the result in the HTML format
  */
- 	// récupération du contenu HTML
-	ob_start();
- 	include(dirname(__FILE__).'/res/exemple07a.php');
- 	include(dirname(__FILE__).'/res/exemple07b.php');
-	$content = ob_get_clean();
 
-	// conversion HTML => PDF
-	require_once(dirname(__FILE__).'/../html2pdf.class.php');
+    // get the HTML
+    ob_start();
+    include(dirname(__FILE__).'/res/exemple07a.php');
+    include(dirname(__FILE__).'/res/exemple07b.php');
+    $content = ob_get_clean();
 
-	try
-	{
-		$html2pdf = new HTML2PDF('P', 'A4', 'fr', false, 'ISO-8859-15');
-		$html2pdf->pdf->SetDisplayMode('fullpage');
-//		$html2pdf->pdf->SetProtection(array('print'), 'spipu');
-		$html2pdf->writeHTML($content, isset($_GET['vuehtml']));
-		$html2pdf->Output('exemple07.pdf');
-	}
-	catch(HTML2PDF_exception $e) { echo $e; }
+    // convert to PDF
+    require_once(dirname(__FILE__).'/../html2pdf.class.php');
+    try
+    {
+        $html2pdf = new HTML2PDF('P', 'A4', 'fr');
+        $html2pdf->pdf->SetDisplayMode('fullpage');
+//      $html2pdf->pdf->SetProtection(array('print'), 'spipu');
+        $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+        $html2pdf->Output('exemple07.pdf');
+    }
+    catch(HTML2PDF_exception $e) {
+        echo $e;
+        exit;
+    }
