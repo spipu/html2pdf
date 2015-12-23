94fa9d8c9ebb75641b870049e310c1c60b7f11dc
Julien Chichignoud <julien.chichignoud@gmail.com>
2015-12-23 19:01:26 +0100

    test the extension system

---
+++ src/Tests/Html2PdfTest.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace Spipu\Html2Pdf\Tests;
+
+use Phake;
+use Spipu\Html2Pdf\Html2Pdf;
+
+/**
+ * Class Html2PdfTest
+ */
+class Html2PdfTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Html2Pdf
+     */
+    private $html2pdf;
+
+    public function setUp()
+    {
+        $this->html2pdf = new Html2Pdf();
+    }
+
+    public function testExtensionTag()
+    {
+        $tag = Phake::mock('Spipu\Html2Pdf\TagInterface');
+        Phake::when($tag)->getName()->thenReturn('test_tag');
+
+        $extension = Phake::mock('Spipu\Html2Pdf\Extension\ExtensionInterface');
+        Phake::when($extension)->getName()->thenReturn('test');
+        Phake::when($extension)->getTags()->thenReturn(array($tag));
+        $this->html2pdf->addExtension($extension);
+
+        $this->html2pdf->writeHTML('<test_tag/>');
+
+        Phake::verify($tag)->open;
+        Phake::verify($tag, Phake::times(2))->close; // TODO Html2Pdf should probably call this only one time
+    }
+}
