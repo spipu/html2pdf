1bdd72007449e4dac027806d4aef62c9d9a47be8
Julien Chichignoud <julien.chichignoud@gmail.com>
2016-04-30 15:15:34 +0200

    remove unused svg features from Html2Pdf class

--- src/Html2Pdf.php
+++ src/Html2Pdf.php
@@ -124,7 +124,6 @@ class Html2Pdf
     protected $_isInTfoot        = false;       // flag : in a tfoot
     protected $_isInOverflow     = false;       // flag : in a overflow
     protected $_isInFooter       = false;       // flag : in a footer
-    protected $_isInDraw         = null;        // flag : in a draw (svg)
     protected $_isAfterFloat     = false;       // flag : is just after a float
     protected $_isInForm         = false;       // flag : is in a float. false / action of the form
     protected $_isInLink         = '';          // flag : is in a link. empty / href of the link
@@ -2301,188 +2300,6 @@ class Html2Pdf
             // solid line : draw directly the polygon
             $this->pdf->Polygon($pt, 'F');
         }
-    }
-
-    /**
-     * prepare a transform matrix, only for drawing a SVG graphic
-     *
-     * @access protected
-     * @param  string $transform
-     * @return array  $matrix
-     */
-    protected function _prepareTransform($transform)
-    {
-        // it can not be  empty
-        if (!$transform) {
-            return null;
-        }
-
-        // sctions must be like scale(...)
-        if (!preg_match_all('/([a-z]+)\(([^\)]*)\)/isU', $transform, $match)) {
-            return null;
-        }
-
-        // prepare the list of the actions
-        $actions = array();
-
-        // for actions
-        for ($k=0; $k<count($match[0]); $k++) {
-
-            // get the name of the action
-            $name = strtolower($match[1][$k]);
-
-            // get the parameters of the action
-            $val = explode(',', trim($match[2][$k]));
-            foreach ($val as $i => $j) {
-                $val[$i] = trim($j);
-            }
-
-            // prepare the matrix, depending on the action
-            switch ($name) {
-                case 'scale':
-                    if (!isset($val[0])) {
-                        $val[0] = 1.;
-
-                    } else {
-                        $val[0] = 1.*$val[0];
-                    }
-                    if (!isset($val[1])) {
-                        $val[1] = $val[0];
-
-                    } else {
-                        $val[1] = 1.*$val[1];
-                    }
-                    $actions[] = array($val[0],0,0,$val[1],0,0);
-                    break;
-
-                case 'translate':
-                    if (!isset($val[0])) {
-                        $val[0] = 0.;
-
-                    } else {
-                        $val[0] = $this->cssConverter->ConvertToMM($val[0], $this->_isInDraw['w']);
-                    }
-                    if (!isset($val[1])) {
-                        $val[1] = 0.;
-
-                    } else {
-                        $val[1] = $this->cssConverter->ConvertToMM($val[1], $this->_isInDraw['h']);
-                    }
-                    $actions[] = array(1,0,0,1,$val[0],$val[1]);
-                    break;
-
-                case 'rotate':
-                    if (!isset($val[0])) {
-                        $val[0] = 0.;
-
-                    } else {
-                        $val[0] = $val[0]*M_PI/180.;
-                    }
-                    if (!isset($val[1])) {
-                        $val[1] = 0.;
-
-                    } else {
-                        $val[1] = $this->cssConverter->ConvertToMM($val[1], $this->_isInDraw['w']);
-                    }
-                    if (!isset($val[2])) {
-                        $val[2] = 0.;
-
-                    } else {
-                        $val[2] = $this->cssConverter->ConvertToMM($val[2], $this->_isInDraw['h']);
-                    }
-                    if ($val[1] || $val[2]) {
-                        $actions[] = array(1,0,0,1,-$val[1],-$val[2]);
-                    }
-                    $actions[] = array(cos($val[0]),sin($val[0]),-sin($val[0]),cos($val[0]),0,0);
-                    if ($val[1] || $val[2]) {
-                        $actions[] = array(1,0,0,1,$val[1],$val[2]);
-                    }
-                    break;
-
-                case 'skewx':
-                    if (!isset($val[0])) {
-                        $val[0] = 0.;
-
-                    } else {
-                        $val[0] = $val[0]*M_PI/180.;
-                    }
-                    $actions[] = array(1,0,tan($val[0]),1,0,0);
-                    break;
-
-                case 'skewy':
-                    if (!isset($val[0])) {
-                        $val[0] = 0.;
-
-                    } else {
-                        $val[0] = $val[0]*M_PI/180.;
-                    }
-                    $actions[] = array(1,tan($val[0]),0,1,0,0);
-                    break;
-                case 'matrix':
-                    if (!isset($val[0])) {
-                        $val[0] = 0.;
-
-                    } else {
-                        $val[0] = $val[0]*1.;
-                    }
-                    if (!isset($val[1])) {
-                        $val[1] = 0.;
-
-                    } else {
-                        $val[1] = $val[1]*1.;
-                    }
-                    if (!isset($val[2])) {
-                        $val[2] = 0.;
-
-                    } else {
-                        $val[2] = $val[2]*1.;
-                    }
-                    if (!isset($val[3])) {
-                        $val[3] = 0.;
-
-                    } else {
-                        $val[3] = $val[3]*1.;
-                    }
-                    if (!isset($val[4])) {
-                        $val[4] = 0.;
-
-                    } else {
-                        $val[4] = $this->cssConverter->ConvertToMM($val[4], $this->_isInDraw['w']);
-                    }
-                    if (!isset($val[5])) {
-                        $val[5] = 0.;
-
-                    } else {
-                        $val[5] = $this->cssConverter->ConvertToMM($val[5], $this->_isInDraw['h']);
-                    }
-                    $actions[] =$val;
-                    break;
-            }
-        }
-
-        // if there are no actions => return
-        if (!$actions) {
-            return null;
-        }
-
-        // get the first matrix
-        $m = $actions[0];
-        unset($actions[0]);
-
-        // foreach matrix => multiply to the last matrix
-        foreach ($actions as $n) {
-            $m = array(
-                $m[0]*$n[0]+$m[2]*$n[1],
-                $m[1]*$n[0]+$m[3]*$n[1],
-                $m[0]*$n[2]+$m[2]*$n[3],
-                $m[1]*$n[2]+$m[3]*$n[3],
-                $m[0]*$n[4]+$m[2]*$n[5]+$m[4],
-                $m[1]*$n[4]+$m[3]*$n[5]+$m[5]
-            );
-        }
-
-        // return the matrix
-        return $m;
     }
 
     /**
@@ -6143,14 +5960,6 @@ class Html2Pdf
         $this->pdf->setXY($x, $y);
 
         // we are in a draw tag
-        // legacy setting
-        $this->_isInDraw = array(
-            'x' => $x,
-            'y' => $y,
-            'w' => $overW,
-            'h' => $overH,
-        );
-
         $this->svgDrawer->startDrawing(
             array(
                 'x' => $x,
@@ -6224,9 +6033,6 @@ class Html2Pdf
         if (!is_null($this->debug)) {
             $this->debug->addStep('DRAW', false);
         }
-
-        // legacy
-        $this->_isInDraw = null;
 
         $this->svgDrawer->stopDrawing();
 
