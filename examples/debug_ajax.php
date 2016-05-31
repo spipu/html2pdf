ad040ef4a6f1b1e8a70a7465b0eeee524772c771
Varrot Olivier <contact@evoweb.fr>
2016-05-31 19:25:28 +0200

    For debug Ajax use

--- examples/debug_ajax.php
+++ examples/debug_ajax.php
@@ -9,19 +9,15 @@
 * @author    Olivier VARROT <contact@evoweb.fr>
 * @copyright 2016 Olivier VARROT
 */
-require_once dirname(__FILE__).'/../vendor/autoload.php';
-
-use Spipu\Html2Pdf\Html2Pdf;
-use Spipu\Html2Pdf\Exception\Html2PdfException;
-use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+require_once(dirname(__FILE__).'/../vendor/autoload.php');
 
 try {
 	ob_start();
 	include dirname(__FILE__).'/res/debug.php';
 	$content = ob_get_clean();
 
-	$html2pdf = new Html2Pdf('P', 'A4', 'fr');
-	$html2pdf->setModeDebug(null,'ajax');
+	$html2pdf = new HTML2PDF('P', 'A4', 'fr');
+	$html2pdf->setModeDebug('ajax');
 	$html2pdf->setDefaultFont('Arial');
 	$html2pdf->writeHTML($content);
 	$html2pdf->Output(__DIR__.'/debug.pdf','F');
