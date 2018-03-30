4de1ca8cc540ba401d2394bb18f1aa1953f0a5d7
Loïc Rentien <loic.rentien@new-immo-group.com>
2018-03-30 12:39:33 +0200

    increase radio/checkbox width

--- src/Html2Pdf.php
+++ src/Html2Pdf.php
@@ -5947,7 +5947,7 @@ class Html2Pdf
 
         switch ($param['type']) {
             case 'checkbox':
-                $w = 3;
+                $w = 4;
                 $h = $w;
                 if ($h<$f) {
                     $y+= ($f-$h)*0.5;
@@ -5957,7 +5957,7 @@ class Html2Pdf
                 break;
 
             case 'radio':
-                $w = 3;
+                $w = 4;
                 $h = $w;
                 if ($h<$f) {
                     $y+= ($f-$h)*0.5;
