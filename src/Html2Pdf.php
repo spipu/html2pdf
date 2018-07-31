67d8d0b80c94404ddd449073a8a4d92fa38ad000
spipu <spipu45@gmail.com>
2018-07-31 15:27:24 +0200

    prepare the next release

--- src/Html2Pdf.php
+++ src/Html2Pdf.php
@@ -242,7 +242,7 @@ class Html2Pdf
         return array(
             'major'     => 5,
             'minor'     => 2,
-            'revision'  => 0
+            'revision'  => 1
         );
     }
 
