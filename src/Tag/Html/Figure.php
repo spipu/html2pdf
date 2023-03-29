7c177cdcdea8b1eda62e90dc9b3d49af6dfbb9d8
Geert Huylebroeck <geert.huylebroeck@piustien.net>
2023-03-29 15:56:38 +0200

    Add Figure HTML tag

---
+++ src/Tag/Html/Figure.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Html2Pdf Library - Tag class
+ *
+ * HTML => PDF converter
+ * distributed under the OSL-3.0 License
+ *
+ * @package   Html2pdf
+ * @author    Laurent MINGUET <webmaster@html2pdf.fr>
+ * @copyright 2017 Laurent MINGUET
+ */
+namespace Spipu\Html2Pdf\Tag\Html;
+
+/**
+ * Tag Figure
+ */
+class Figure extends Span
+{
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'figure';
+    }
+}
